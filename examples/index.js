e2289cefc09e58329f32944444fad519c88f98b9
fmvilas <fmvilas@gmail.com>
2017-03-24 19:13:59 +0100

    Added reconnection support

--- examples/index.js
+++ examples/index.js
@@ -2,11 +2,15 @@ const { TopicMQ } = require('../lib');
 
 // Check http://www.squaremobius.net/amqp.node/channel_api.html to see all options.
 const mq = new TopicMQ({
-  exchange: 'hitch',
-  queue_options: { exclusive: true },
+  exchange: 'logs',
+  queue_options: {
+    exclusive: false,
+    durable: true,
+    autoDelete: true
+  },
   consumer_options: { noAck: true },
-  topic: 'v1.user.signup',
-  queue: 'test',
+  topic: '#',
+  queue: 'my.queue.name',
   username: process.env.USERNAME,
   password: process.env.PASSWORD,
   host: process.env.HOST || 'localhost',
@@ -18,8 +22,16 @@ mq.on('connect', () => {
   console.log('connected');
 });
 
-mq.on('consume', (msg) => {
-  console.log(msg.content.toString());
+mq.on('reconnecting', (info) => {
+  console.log('reconnecting:', info.attempts);
+});
+
+mq.on('reconnect', (info) => {
+  console.log('reconnected on attempt', info.attempts);
+});
+
+mq.on('message', (msg) => {
+  console.log(JSON.parse(msg.content.toString()));
 });
 
 mq.connect();
